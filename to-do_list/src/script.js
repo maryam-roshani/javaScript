865b7437a463e57a3a995f6a2b1a431fb90dc9bb
Maryam Roshani <maryamroshani777@gmail.com>
2024-05-19 16:29:49 +0430

    to-do list

--- to-do_list/src/script.js
+++ to-do_list/src/script.js
@@ -4,14 +4,24 @@ let taskList = document.getElementById("task-list")
 
 function addTask() {
     if (taskInput.value !== "") {
-        console.log(taskInput.value)
         let newTask = document.createElement('li')
-        let taskDef = document.createElement('p')
-        taskDef.innerText = taskInput.value
-        taskList.appendChild(newTask).appendChild(taskDef)
+        newTask.innerHTML = taskInput.value
+        taskList.appendChild(newTask)
+        let image = document.createElement('img')
+        image.src = 'assets/x.svg';
+        newTask.appendChild(image)
     }
+    taskInput.value = "";
 }
 
 
 
 addButton.addEventListener('click', addTask)
+
+taskList.addEventListener('click', function(e) {
+    if (e.target.tagName === "LI") {
+        e.target.classList.toggle("checked");
+    } else if (e.target.tagName === "IMG") {
+        e.target.parentElement.remove();
+    }
+}, false)
