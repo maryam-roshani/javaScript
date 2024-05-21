11e192a8855e5cec0a154cb191e277cda2a20252
Maryam Roshani <maryamroshani777@gmail.com>
2024-05-21 23:11:35 +0430

    music player

---
+++ image search engine/src/script.js
@@ -0,0 +1,4 @@
+let searchInput = document.getElementById("search-input")
+let searchButton = document.getElementById("search-button")
+let result = document.getElementsByClassName("result")
+let showMore = document.getElementsByClassName("show-more")
