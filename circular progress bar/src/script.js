38ac43e9971f612170cbde7fde0ad196b3b3ea9f
Maryam Roshani <maryamroshani777@gmail.com>
2024-06-03 07:07:16 +0430

    waiting dots

--- circular progress bar/src/script.js
+++ circular progress bar/src/script.js
@@ -1,10 +1,18 @@
 function waiting() {
     const dots = document.getElementsByClassName('dots')
-    for (const item of dots) {
-        item.classList.add('larger')
-        setTimeout(() => {
+        // for (let item of dots) {
+        //     setInterval(() => {
+        //         item.classList.add('larger')
+        //     }, 500)
+        // }
+    for (let i = 0; i < dots.length; i++) {
+        task(i);
+    }
 
-        }, 500)
+    function task(i) {
+        setTimeout(() => {
+            dots[i].classList.add('larger')
+        }, 500 * i);
     }
 }
 
